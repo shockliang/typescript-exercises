a4d767767b370dfc2097cfeaa57aed1275a1419b
shocklaing <shockwater@gmail.com>
2017-09-23 15:43:12 +0800

    Access modifers in constructor paramaters

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -2,6 +2,8 @@ var Point = /** @class */ (function () {
     function Point(x, y) {
         this.x = x;
         this.y = y;
+        this.x = x;
+        this.y = y;
     }
     Point.prototype.draw = function () {
         console.log('X: ' + this.x + ' Y: ' + this.y);
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,9 +1,6 @@
 
 class Point {
-    private x: number;
-    private y: number;
-
-    constructor(x?: number, y?: number) {
+    constructor(private x?: number, private y?: number) {
         this.x = x;
         this.y = y;
     }
