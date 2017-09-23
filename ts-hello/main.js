48b5efe936cb4c073c7774ada773e7c0213e147e
shocklaing <shockwater@gmail.com>
2017-09-23 15:57:42 +0800

    Properties exercise

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,14 +1,25 @@
 var Point = /** @class */ (function () {
-    function Point(x, y) {
-        this.x = x;
-        this.y = y;
-        this.x = x;
-        this.y = y;
+    function Point(_x, _y) {
+        this._x = _x;
+        this._y = _y;
     }
     Point.prototype.draw = function () {
-        console.log('X: ' + this.x + ' Y: ' + this.y);
+        console.log('X: ' + this._x + ' Y: ' + this._y);
     };
+    Object.defineProperty(Point.prototype, "x", {
+        get: function () {
+            return this._x;
+        },
+        set: function (value) {
+            if (value < 0)
+                throw new Error('Value cannot be less than 0');
+            this._x = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return Point;
 }());
 var point = new Point(1, 2);
+var x = point.x;
 point.draw();
