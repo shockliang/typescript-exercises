b050cb3faadaa9eebc630035a4ae37269b513e28
shocklaing <shockwater@gmail.com>
2018-05-14 23:18:35 +0800

    Setup ts config for vs code.

---
+++ vs-code-init/app.js
@@ -0,0 +1,11 @@
+"use strict";
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log('Init vs code for ts.');
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
---
+++ vs-code-init/app.ts
@@ -0,0 +1,8 @@
+class Startup {
+    public static main(): number {
+        console.log('Init vs code for ts.');
+        return 0;
+    }
+}
+
+Startup.main();
