3ddf0efc231d7cb91688652967826457e4abf319
shocklaing <shockwater@gmail.com>
2018-05-14 23:31:33 +0800

    Type inference.

---
+++ types/type-inference.js
@@ -0,0 +1,13 @@
+"use strict";
+var car = {
+    brand: "BMW",
+    engine: 3.4,
+    run: function () {
+        console.log("Driving");
+    }
+};
+car.brand = "Mercedes";
+// Car.brand = 3.4;
+car.engine = 3.5;
+// car.engine = "3.5";
+// car.run = 'test func'; 
---
+++ types/type-inference.ts
@@ -0,0 +1,15 @@
+let car = {
+    brand: "BMW",
+    engine: 3.4,
+    run: function() {
+        console.log("Driving");
+    }
+}
+
+car.brand = "Mercedes";
+// Car.brand = 3.4;
+
+car.engine = 3.5;
+// car.engine = "3.5";
+
+// car.run = 'test func';
