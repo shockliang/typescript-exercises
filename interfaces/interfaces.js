a5a0a7ed36ec0d761d33f4d32652a623d5fafce2
shocklaing <shockwater@gmail.com>
2018-05-17 22:16:40 +0800

    Implementing interface to functions.

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -6,3 +6,7 @@ var automobile = {
         console.log("this " + this.brand + " is going at " + this.speed + " miles an hour");
     }
 };
+function car1(automobile) {
+    console.log("this " + automobile.brand + " is going at " + automobile.speed + " miles an hour");
+}
+car1(automobile);
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -11,3 +11,12 @@ const automobile: AutomobileInterface = {
     console.log(`this ${this.brand} is going at ${this.speed} miles an hour`);
   }
 };
+
+function car1(automobile: AutomobileInterface) {
+//   automobile.brand = 3.14;
+  console.log(
+    `this ${automobile.brand} is going at ${automobile.speed} miles an hour`
+  );
+}
+
+car1(automobile);
