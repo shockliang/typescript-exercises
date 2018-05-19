a9380ccf60f24c360a1a01b94a6c16ea9d8ed936
shocklaing <shockwater@gmail.com>
2018-05-19 16:53:11 +0800

    Generics.

---
+++ using-classes/generics.js
@@ -0,0 +1,7 @@
+"use strict";
+function displayData(someData) {
+    return someData;
+}
+console.log(displayData("Hello"));
+console.log(displayData(3.1415));
+console.log(displayData(false));
---
+++ using-classes/generics.ts
@@ -0,0 +1,9 @@
+function displayData<T>(someData: T) {
+  return someData;
+}
+
+console.log(displayData("Hello"));
+console.log(displayData(3.1415));
+console.log(displayData(false));
+
+
